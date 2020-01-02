2c71556e1298a4f8bfb980fdd10ef143fd19f379
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2020-01-02 22:09:18 +0200

    Write on Promise and add createTable and insertRow methods

--- db_class.js
+++ db_class.js
@@ -4,7 +4,7 @@ const sqlite3 = require("sqlite3").verbose();
  * Class representing a database connection.
  * @param {object} _db - Protected property.
  */
-module.exports = class DB {
+class DB {
     /**
      * @constructor
      * @param {string} dbName - Relative address to database file.
@@ -18,18 +18,22 @@ module.exports = class DB {
             console.log(`Connected to the "${dbName}" database!`);
         });
     }
+
     /**
      * @method dbClose
      * @description Close database connection.
      */
     dbClose() {
-        this._db.close((err) => {
-            if (err) {
-                return console.error(err.message);
-            }
-            console.log('Close the database connection!');
+        return new Promise((resolve, reject) => {
+            this._db.close((err) => {
+                if (err) {
+                    reject(err.message);
+                }
+                resolve(true);
+            });
         });
     }
+
     /**
      * @method all
      * @param {string} table - Name of table in database.
@@ -37,12 +41,72 @@ module.exports = class DB {
      * @description Get all data from selecte table in database.
      */
     all(table) {
-        const query = `SELECT * FROM ${table}`;
-        this._db.all(query, [], (err, rows) => {
-            if (err) {
-                return console.error(err.message);
-            }
-            return rows;
+        return new Promise((resolve, reject) => {
+            const query = `SELECT * FROM ${table}`;
+            this._db.all(query, [], (err, rows) => {
+                if (err) {
+                    reject(err.message);
+                }
+                resolve(rows);
+            });
+        });
+    }
+
+    /**
+     * @method getOne
+     * @param {string} tableName - Name of table.
+     * @param {string} param - Column name in the table with given data.
+     * @return {object} One row from table.
+     * @description Get one row data from table.
+     */
+    getOne(tableName, param) {
+        return new Promise((resolve, reject) => {
+            const query = `SELECT * FROM ${tableName} WHERE ${param}`;
+            this._db.get(query, [], (err, row) => {
+                if (err) {
+                    reject(err.message);
+                }
+                resolve(row);
+            });
+        });
+    }
+
+    /**
+     * @method insertRow
+     * @param {string} table - Name of table and column in table.
+     * @param {string} values - Values for insert to table.
+     * @return {boolean} Return true or error.
+     * @description Insert one row into the table.
+     */
+    insertRow(table, values) {
+        return new Promise((resolve, reject) => {
+            const sql = `INSERT INTO ${table} VALUES(${values})`;
+            this._db.run(sql, [], err => {
+                if (err) {
+                    reject(err.message);
+                }
+                resolve(true);
+            });
+        });
+    }
+
+    /**
+     * @method createTable
+     * @param {string} table - Name of created table.
+     * @param {string} params - Columns name and parameters.
+     * @return {boolean} Return true or error.
+     * @descriptioin Create new table in database.
+     */
+    createTable(table, params) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`CREATE TABLE ${table} (${params})`, err => {
+                if (err) {
+                    reject(err.message);
+                }
+                resolve(true);
+            });
         });
     }
 }
+
+module.exports = DB;
--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const DB = require('./db_class');
 
 const db = new DB('./database/node.db');
-db.all("langs");
-db.dbClose();
 
-// db.run('CREATE TABLE langs(name text)');
+db.all("users")
+	.then(a => console.log(a))
+	.catch(e => console.error(e));
 
-// insert one row into the langs table
-// db.run(`INSERT INTO langs(name) VALUES(?)`, ['C'], function(err) {
-// 	if (err) {
-// 		return console.error(err.message);
-// 	}
-// 	// get the last insert id
-// 	console.log(`A row has been inserted with rowid ${this.lastID} and ${this.changes}`);
-// });
+db.getOne("langs", "name='Andrew'")
+	.then(response => console.log(response))
+	.catch(e => console.error(e));
+
+const table = "greetings(message, author)";
+const data = "'Hello from world!', 'Dane'";
+db.insertRow(table, data)
+	.then(result => console.log('A row has been inserted!'))
+	.catch(e => console.error(e));
+
+const newTable = "greetings";
+const newParam = "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, message TEXT, author TEXT";
+// db.createTable(newTable, newParam)
+// 	.then(result => console.log('New table has created!'))
+// 	.catch(e => console.error(e));
+
+db.dbClose().then(response => console.log('Close the database connection!')).catch(e => console.error(e));
